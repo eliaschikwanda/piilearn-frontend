7886d26da66a3f0596fa6fcd7c1d0a5af7bd08c5
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-02 04:36:29 +0200

    Added the dashboard and questionlist component in the routes

--- piilearn-web-ui/src/app/modules/question/question-routing.module.ts
+++ piilearn-web-ui/src/app/modules/question/question-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./pages/main/main.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {QuestionListComponent} from "./pages/question-list/question-list.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    // To display everything in main component have a list of children
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        component: QuestionListComponent
+      }
+    ]
   }
 ];
 
