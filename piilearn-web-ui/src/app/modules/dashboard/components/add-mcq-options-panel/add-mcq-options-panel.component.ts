cd1ebb88ab3147f138c16e9e752ac0affac0e230
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-04 19:19:44 +0200

    Added question number and question description

--- piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-options-panel/add-mcq-options-panel.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-options-panel/add-mcq-options-panel.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {McqQuestionRequest} from "../../../../services/models/mcq-question-request";
+import {DividerModule} from "primeng/divider";
+import {PanelModule} from "primeng/panel";
 
 @Component({
   selector: 'app-add-mcq-options-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    DividerModule,
+    PanelModule
+  ],
   templateUrl: './add-mcq-options-panel.component.html',
   styleUrl: './add-mcq-options-panel.component.scss'
 })
--- piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-question-panel/add-mcq-question-panel.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-question-panel/add-mcq-question-panel.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {DividerModule} from "primeng/divider";
+import {PanelModule} from "primeng/panel";
+import {McqQuestionRequest} from "../../../../services/models/mcq-question-request";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {FormsModule} from "@angular/forms";
+import {InputNumberModule} from "primeng/inputnumber";
 
 @Component({
   selector: 'app-add-mcq-question-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    DividerModule,
+    PanelModule,
+    FloatLabelModule,
+    InputTextareaModule,
+    FormsModule,
+    InputNumberModule
+  ],
   templateUrl: './add-mcq-question-panel.component.html',
   styleUrl: './add-mcq-question-panel.component.scss'
 })
 export class AddMcqQuestionPanelComponent {
-
+  mcqQuestionRequest: McqQuestionRequest = {
+    id: 0,
+    mcqQuestionDescription: '',
+    mcqQuestionNumOnOriginalPaper: 0,
+    seasonId: 0,
+    subjectId: 0,
+    variantId: 0,
+    yearId: 0
+  }
 }
