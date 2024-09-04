b00d38c104b3392a8d02ae24e80f8bc544d9777a
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-04 11:14:54 +0200

    Added stripes to the question list

--- piilearn-web-ui/src/app/modules/question/components/mcq-question-table/mcq-question-table.component.ts
+++ piilearn-web-ui/src/app/modules/question/components/mcq-question-table/mcq-question-table.component.ts
@@ -3,6 +3,8 @@ import {PageResponseMcqQuestionResponse} from "../../../../services/models/page-
 import {PrimeTemplate} from "primeng/api";
 import {TableModule} from "primeng/table";
 import {TruncateTextComponent} from "../truncate-text/truncate-text.component";
+import {StyleClassModule} from "primeng/styleclass";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-mcq-question-table',
@@ -10,11 +12,14 @@ import {TruncateTextComponent} from "../truncate-text/truncate-text.component";
   imports: [
     PrimeTemplate,
     TableModule,
-    TruncateTextComponent
+    TruncateTextComponent,
+    StyleClassModule,
+    NgClass
   ],
   templateUrl: './mcq-question-table.component.html',
   styleUrl: './mcq-question-table.component.scss'
 })
 export class McqQuestionTableComponent {
   @Input() public mcqQuestionResponse: PageResponseMcqQuestionResponse = {};
+
 }
