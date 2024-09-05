264b38afcd70282b8e7317b24ba629ee678ac604
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-05 20:26:32 +0200

    Implemented the teh saveOption questions

--- piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-options-panel/add-mcq-options-panel.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-options-panel/add-mcq-options-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DividerModule} from "primeng/divider";
 import {PanelModule} from "primeng/panel";
 import {NgForOf, NgIf} from "@angular/common";
@@ -11,6 +11,10 @@ import {InputTextModule} from "primeng/inputtext";
 import {Button} from "primeng/button";
 import {CardModule} from "primeng/card";
 import {MathJaxParagraphComponent} from "../math-jax-paragraph/math-jax-paragraph.component";
+import {QuestionIdSaveService} from "../../../../services/shared/questIdSave/questionIdSave.service";
+import {McqOptionFrontend} from "../../../../services/models/mcq-option-frontend";
+import {ERROR_MESSAGES} from "../../../../../error-message";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-add-mcq-options-panel',
@@ -31,8 +35,9 @@ import {MathJaxParagraphComponent} from "../math-jax-paragraph/math-jax-paragrap
   templateUrl: './add-mcq-options-panel.component.html',
   styleUrl: './add-mcq-options-panel.component.scss'
 })
-export class AddMcqOptionsPanelComponent {
+export class AddMcqOptionsPanelComponent implements OnInit{
   errorMsg: string[] = [];
+  mcqQuestionOptionIds: number[] = [];
   optionDescription: string = "CH\\(_{3}\\,^{+}\\)";
   mcqOptionRequest: McqOptionRequest = {
     id: 0,
@@ -41,16 +46,58 @@ export class AddMcqOptionsPanelComponent {
     mcqQuestionId: 0
   }
 
+  mcqOptionFrontend: McqOptionFrontend = {};
+  private questionIdSubscription!: Subscription;
+
   constructor(
-    private mcqOptionService: MultipleChoiceOptionsService
+    private mcqOptionService: MultipleChoiceOptionsService,
+    private questionIdSave: QuestionIdSaveService
   ) {
   }
 
+  // Put all the code you want after Angular is done creating components
+  ngOnInit(): void {
+    this.questionIdSubscription = this.questionIdSave.questionId$.subscribe(id => {
+      this.mcqOptionRequest.mcqQuestionId = id;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.questionIdSubscription) {
+      this.questionIdSubscription.unsubscribe();
+    }
+  }
+
   saveOption() {
-    console.log(this.mcqOptionRequest)
+    this.errorMsg = []
+    // Input will be validated by the backend
+    this.mcqOptionService.createMcqOption({
+      body: this.mcqOptionRequest
+    }).subscribe({
+      next: (res) => {
+        this.addOrUpdateOption(res);
+        this.mcqQuestionOptionIds.push(res);
+      },
+      error: (err) => {
+        if (err.error.validationErrors) {
+          err.error.validationErrors.forEach((msg: string ) => {
+            this.errorMsg.push(ERROR_MESSAGES[msg] || msg);
+          });
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      }
+    })
   }
 
   onQuestionOptionChange(optionDescription: string) {
     this.optionDescription = optionDescription;
+  }
+
+  addOrUpdateOption(res: number) {
+    this.mcqOptionFrontend[res] = {
+      mcqOptionRep: this.mcqOptionRequest.optionRep,
+      mcqOptionDes: this.mcqOptionRequest.optionDescription,
+    };
   }
 }
