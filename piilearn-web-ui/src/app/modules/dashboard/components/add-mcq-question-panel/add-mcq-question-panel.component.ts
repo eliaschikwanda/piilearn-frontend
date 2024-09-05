a8e46427ee62466697d9960c1be1dfcaecf6d65d
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-05 07:37:57 +0200

    Completed the question addition form

--- piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-question-panel/add-mcq-question-panel.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/components/add-mcq-question-panel/add-mcq-question-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DividerModule} from "primeng/divider";
 import {PanelModule} from "primeng/panel";
 import {McqQuestionRequest} from "../../../../services/models/mcq-question-request";
@@ -6,6 +6,19 @@ import {FloatLabelModule} from "primeng/floatlabel";
 import {InputTextareaModule} from "primeng/inputtextarea";
 import {FormsModule} from "@angular/forms";
 import {InputNumberModule} from "primeng/inputnumber";
+import {SubjectResponse} from "../../../../services/models/subject-response";
+import {VariantResponse} from "../../../../services/models/variant-response";
+import {SeasonResponse} from "../../../../services/models/season-response";
+import {YearResponse} from "../../../../services/models/year-response";
+import {YearService} from "../../../../services/services/year.service";
+import {SeasonService} from "../../../../services/services/season.service";
+import {SubjectsService} from "../../../../services/services/subjects.service";
+import {VariantService} from "../../../../services/services/variant.service";
+import {DropdownModule} from "primeng/dropdown";
+import {ButtonModule} from "primeng/button";
+import {NgForOf, NgIf} from "@angular/common";
+import {MultipleChoiceQuestionsService} from "../../../../services/services/multiple-choice-questions.service";
+import {ERROR_MESSAGES} from "../../../../../error-message";
 
 @Component({
   selector: 'app-add-mcq-question-panel',
@@ -16,12 +29,17 @@ import {InputNumberModule} from "primeng/inputnumber";
     FloatLabelModule,
     InputTextareaModule,
     FormsModule,
-    InputNumberModule
+    InputNumberModule,
+    DropdownModule,
+    ButtonModule,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './add-mcq-question-panel.component.html',
   styleUrl: './add-mcq-question-panel.component.scss'
 })
-export class AddMcqQuestionPanelComponent {
+export class AddMcqQuestionPanelComponent implements OnInit{
+  errorMsg: string[] = [];
   mcqQuestionRequest: McqQuestionRequest = {
     id: 0,
     mcqQuestionDescription: '',
@@ -30,5 +48,130 @@ export class AddMcqQuestionPanelComponent {
     subjectId: 0,
     variantId: 0,
     yearId: 0
+  }
+
+  // Arrays with data to select from
+  seasons!: SeasonResponse[];
+  examSubjects!: SubjectResponse[];
+  variants!: VariantResponse[];
+  years!: YearResponse[];
+
+  // Variables with selected data
+  selectedSeason: SeasonResponse | undefined;
+  selectedSubject: SubjectResponse | undefined;
+  selectedVariant: VariantResponse | undefined;
+  selectedYear: YearResponse | undefined;
+
+  // SavedEntities
+  private savedQuestionId: number | undefined;
+
+  constructor(
+    private seasonService: SeasonService,
+    private subjectService: SubjectsService,
+    private variantService: VariantService,
+    private yearService: YearService,
+    private mcqQuestionService: MultipleChoiceQuestionsService
+  ) {
+  }
+
+  ngOnInit(): void {
+    // Fetch the seasons
+    this.seasonService.getAllSeasons({
+      // no params
+    }).subscribe({
+      next: (seasons) => {
+        this.seasons = seasons;
+      },
+      error: (err) => {
+        this.errorMsg.push("Can not extract seasons at the moment.")
+      }
+    });
+
+    // Fetch the subjects
+    this.subjectService.getAllSubjects({
+      // no params
+    }).subscribe({
+      next: (subjects) => {
+        this.examSubjects = subjects;
+      },
+      error: (err) => {
+        this.errorMsg.push("Can not extract subjects at the moment.")
+      }
+    })
+
+    // Fetch the variant
+    this.variantService.getAllVariants({
+      // no params
+    }).subscribe({
+      next: (variants) => {
+        this.variants = variants;
+      },
+    error: (err) => {
+        this.errorMsg.push("Can not extract variants at the moment.")
+    }
+    })
+
+    // Fetch the years
+    this.yearService.getAllYears({
+      // no params
+    }).subscribe({
+      next: (years) => {
+        this.years = years;
+      },
+      error: (err) => {
+        this.errorMsg.push("Can not extract years at the moment.")
+      }
+    });
+
+
+  }
+
+  saveQuestion() {
+    this.errorMsg = [];
+    if (this.inputIsValid()) {
+      this.mcqQuestionService.createMcqQuestion({
+        body: this.mcqQuestionRequest
+      }).subscribe({
+        next: (res) => {
+          this.savedQuestionId = res;
+        },
+        error: (err) => {
+          if (err.error.validationErrors) {
+            err.error.validationErrors.forEach((msg: string ) => {
+              this.errorMsg.push(ERROR_MESSAGES[msg] || msg);
+            });
+          } else {
+            this.errorMsg.push(err.error.error);
+          }
+          console.log(err)
+        }
+      })
+    }
+  }
+
+  private inputIsValid() {
+    let isValid: boolean = true
+    if (this.selectedYear == undefined) {
+      this.errorMsg.push("Year is required.");
+      isValid = false;
+    }
+    if (this.selectedVariant == undefined) {
+      this.errorMsg.push("Variant is required.");
+      isValid = false;
+    }
+    if (this.selectedSubject == undefined) {
+      this.errorMsg.push("Subject is required.")
+      isValid = false;
+    }
+    if (this.selectedSeason == undefined) {
+      this.errorMsg.push("Season is required.")
+      isValid = false;
+    }
+    if (this.mcqQuestionRequest.mcqQuestionNumOnOriginalPaper < 1
+    || this.mcqQuestionRequest.mcqQuestionNumOnOriginalPaper > 40) {
+      this.errorMsg.push("The number on original question should be between 1 and 40 inclusive.")
+      isValid = false;
+    }
+    return isValid;
   }
 }
--- piilearn-web-ui/src/app/services/global-error-handler.service.ts
+++ piilearn-web-ui/src/app/services/global-error-handler.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {ErrorHandler, Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalErrorHandlerService {
+export class GlobalErrorHandlerService implements ErrorHandler{
 
   constructor() { }
+
+  handleError(error: any): void {
+    console.log('An error occurred', error)
+  }
 }
