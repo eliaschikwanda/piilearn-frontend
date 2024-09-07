45b7e03753ec04151fc75061590650298146bccb
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-07 12:05:22 +0200

    Dashboard side menu starter files

---
+++ piilearn-web-ui/src/app/modules/dashboard/components/dashboard-sidebar-menu/dashboard-sidebar-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sidebar-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-sidebar-menu.component.html',
+  styleUrl: './dashboard-sidebar-menu.component.scss'
+})
+export class DashboardSidebarMenuComponent {
+
+}
