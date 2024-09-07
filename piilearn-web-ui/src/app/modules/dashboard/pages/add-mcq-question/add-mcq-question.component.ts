143ba930b376cf1f5ac7075f5fc515ce60eff3fc
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-07 13:39:39 +0200

    Changed to bootstrap button

--- piilearn-web-ui/src/app/modules/dashboard/pages/add-mcq-question/add-mcq-question.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/pages/add-mcq-question/add-mcq-question.component.ts
@@ -3,16 +3,18 @@ import {SplitterModule} from "primeng/splitter";
 import {AddMcqQuestionPanelComponent} from "../../components/add-mcq-question-panel/add-mcq-question-panel.component";
 import {AddMcqOptionsPanelComponent} from "../../components/add-mcq-options-panel/add-mcq-options-panel.component";
 import {AddAnswerToMcqPanelComponent} from "../../components/add-answer-to-mcq-panel/add-answer-to-mcq-panel.component";
+import {DashboardSidebarMenuComponent} from "../../components/dashboard-sidebar-menu/dashboard-sidebar-menu.component";
 
 @Component({
   selector: 'app-add-mcq-question',
   standalone: true,
-  imports: [
-    SplitterModule,
-    AddMcqQuestionPanelComponent,
-    AddMcqOptionsPanelComponent,
-    AddAnswerToMcqPanelComponent
-  ],
+    imports: [
+        SplitterModule,
+        AddMcqQuestionPanelComponent,
+        AddMcqOptionsPanelComponent,
+        AddAnswerToMcqPanelComponent,
+        DashboardSidebarMenuComponent
+    ],
   templateUrl: './add-mcq-question.component.html',
   styleUrl: './add-mcq-question.component.scss'
 })
--- piilearn-web-ui/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {DashboardSidebarMenuComponent} from "../../components/dashboard-sidebar-menu/dashboard-sidebar-menu.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [
-    DashboardSidebarMenuComponent
-  ],
+    imports: [
+        DashboardSidebarMenuComponent,
+        RouterOutlet
+    ],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.scss'
 })
