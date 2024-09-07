7535ce119cac4a1f5341368f6b0d788576747b27
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-07 13:39:26 +0200

    Changed the theme to dark mode

--- piilearn-web-ui/src/app/modules/dashboard/components/dashboard-sidebar-menu/dashboard-sidebar-menu.component.ts
+++ piilearn-web-ui/src/app/modules/dashboard/components/dashboard-sidebar-menu/dashboard-sidebar-menu.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {SidebarModule} from "primeng/sidebar";
+import {Button} from "primeng/button";
+import {Route, Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-sidebar-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarModule,
+    Button
+  ],
   templateUrl: './dashboard-sidebar-menu.component.html',
   styleUrl: './dashboard-sidebar-menu.component.scss'
 })
 export class DashboardSidebarMenuComponent {
+  sidebarVisible: boolean = false;
 
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  goToAddMcqQuestion() {
+    this.router.navigate(["dashboard/add-mcq-question"])
+  }
 }
