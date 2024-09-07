d98518cc75edec7ffbb8e6d3b3a208b3264c88b9
eliaschikwanda <chikwandaelias@gmail.com>
2024-09-07 16:20:17 +0200

    Can get the passes paramter

--- piilearn-web-ui/src/app/modules/question/pages/view-mcq-question/view-mcq-question.component.ts
+++ piilearn-web-ui/src/app/modules/question/pages/view-mcq-question/view-mcq-question.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {SplitterModule} from "primeng/splitter";
 import {Panel1ViewMcqComponent} from "../../components/panel-1-view-mcq/panel-1-view-mcq.component";
 import {Panel2ViewMcqComponent} from "../../components/panel-2-view-mcq/panel-2-view-mcq.component";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-view-mcq-question',
@@ -15,5 +16,17 @@ import {Panel2ViewMcqComponent} from "../../components/panel-2-view-mcq/panel-2-
   styleUrl: './view-mcq-question.component.scss'
 })
 export class ViewMcqQuestionComponent {
+  mcqQuestionCode: string | null= "";
 
+  constructor(
+    private route: ActivatedRoute,
+  ) {
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.mcqQuestionCode = params.get('mcq-question-code');
+      console.log(this.mcqQuestionCode);
+    })
+  }
 }
